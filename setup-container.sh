2a901957dccf581d8fef69451b4303233d51e2a9
n-jay <nuwanjayawardene@gmail.com>
2026-03-20 18:46:55 -0600

    Add support for specifying container engine in setup script

--- setup-container.sh
+++ setup-container.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-./run-container.sh bash -c "echo start && cd /data && echo 'hello' && ./setup.sh"
+ENGINE_ARG=""
+if [ "$1" = "--docker" ] || [ "$1" = "--podman" ]; then
+    ENGINE_ARG="$1"
+    shift
+fi
+
+./run-container.sh $ENGINE_ARG bash -c "echo start && cd /data && echo 'hello' && ./setup.sh"

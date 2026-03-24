7b9afa04d27dd7808c939d40a8ad75a0cb346dc6
Everett Lewark <78086132+lewark@users.noreply.github.com>
2026-03-24 01:20:18 -0600

    Add result plotting script

---
+++ llm-j-scripts/plot_results.py
@@ -0,0 +1,74 @@
+import json
+import numpy
+import os
+import re
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+pattern = re.compile(r"llm-j-parsed_([a-z0-9\._\-]+)_(\d{4}-\d{2}-\d{2}_\d{6})\.json")
+
+
+def get_model_scores(directory: str):
+    scores: dict[str, list[float]] = {}
+
+    for filename in os.listdir(directory):
+        match = pattern.match(filename)
+
+        model_name = "unknown"
+        if match is not None:
+            model_name = match.group(1)
+
+        if model_name not in scores:
+            scores[model_name] = []
+        model_scores = scores[model_name]
+
+        file_path = os.path.join(directory, filename)
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+
+        for item in data:
+            score = item["llmj_analysis"]["score"]
+            model_scores.append(score)
+
+    return scores
+
+
+def get_mean_sd(model_scores: dict[str, list[float]]) -> pd.DataFrame:
+    model_names = sorted(model_scores.keys())
+    rows = []
+    for name in model_names:
+        scores = model_scores[name]
+        rows.append([
+            name,
+            np.mean(scores),
+            np.std(scores),
+            len(scores)
+        ])
+
+    return pd.DataFrame(rows, columns=["model", "mean", "sd", "count"])
+
+
+def plot_model_scores(model_scores: dict[str, list[float]]) -> None:
+    names = sorted(model_scores.keys())
+    scores = [model_scores[name] for name in names]
+    plt.boxplot(scores, tick_labels=names)
+    plt.savefig("model_scores.svg")
+    plt.savefig("model_scores.png")
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please enter a directory name")
+    dir_name = sys.argv[1]
+
+    model_scores = get_model_scores(dir_name)
+    print(get_mean_sd(model_scores))
+
+    plot_model_scores(model_scores)
+
+
+if __name__ == "__main__":
+    main()

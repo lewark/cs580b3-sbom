83da4e3f0318804ed3ffc3a542e37c698d146812
Everett Lewark <78086132+lewark@users.noreply.github.com>
2026-03-29 03:10:31 -0600

    Remove old container setup script

--- setup-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ENGINE_ARG=""
-if [ "$1" = "--docker" ] || [ "$1" = "--podman" ]; then
-    ENGINE_ARG="$1"
-    shift
-fi
-
-./run-container.sh $ENGINE_ARG bash -c "echo start && cd /data && echo 'hello' && ./setup.sh"

4f9b52d9ad42cd1b68f968e86c9a05ead52926bb
n-jay <nuwanjayawardene@gmail.com>
2026-04-05 15:28:20 -0600

    Add additional software entries to grid configuration

--- grid_config.sh
+++ grid_config.sh
@@ -10,6 +10,10 @@ MODELS=(
 # Associative array of software to test, format: ["name"]="url"
 declare -A SOFTWARE=(
     ["tomcat"]="https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.57/src/apache-tomcat-8.5.57-src.tar.gz"
+    ["continuum"]="https://archive.apache.org/dist/continuum/binaries/apache-continuum-1.4.2-bin.tar.gz"
+    ["archiva"]="https://archive.apache.org/dist/archiva/2.0.1/binaries/apache-archiva-2.0.1-bin.tar.gz"
+    ["log4j"]="https://archive.apache.org/dist/logging/log4j/1.2.17/log4j-1.2.17.tar.gz"
+    ["wink"]="https://archive.apache.org/dist/logging/log4j/1.2.17/log4j-1.2.17.tar.gz"
 )
 
 # Number of iterations to run for each model/software combination

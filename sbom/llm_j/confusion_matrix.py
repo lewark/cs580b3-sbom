5e5eac33b228d77c303c2365f74cc8869fa86543
lewark <78086132+lewark@users.noreply.github.com>
2026-04-12 20:47:33 -0600

    Split confusion matrices by model

--- sbom/llm_j/confusion_matrix.py
+++ sbom/llm_j/confusion_matrix.py
@@ -6,7 +6,10 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
+
+from sbom.paths import find_json_files
 
 from .vulnrichment import get_vulnrichment_data
 
@@ -17,42 +20,45 @@ INDEXES = { text: i for i, text in enumerate(LABELS) }
 MISSION_WELL_BEING = "medium"
 
 
-def get_confusion_matrix(directory: str):
-    confusion_matrix = np.zeros((5, 4))
+def get_confusion_matrices(directory: str) -> dict[str, npt.NDArray]:
+    confusion_matrices = {}
 
     decision_tree = load_decision_tree()
 
-    for dirpath, dirnames, filenames in os.walk(directory):
-        for filename in filenames:
-            if not filename.endswith(".json"):
+    for file_path in find_json_files(directory, required_strs=["parsed-logs"], excluded_strs=["llm-j-analysis-logs", "iteration4", "iteration5"]):
+        filename = os.path.basename(file_path)
+        if not filename.endswith(".json"):
+            continue
+
+        match = pattern.match(filename)
+
+        if match is None:
+            continue
+        model_name = match.group(1)
+
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+
+        if "vulnerabilities" not in data:
+            continue
+
+        if model_name not in confusion_matrices:
+            confusion_matrices[model_name] = np.zeros((5, 4))
+        confusion_matrix = confusion_matrices[model_name]
+
+        for item in data["vulnerabilities"]:
+            vuln_id = item["vulnerability_id"]
+            predicted_decision = item["ssvc_decision"]
+
+            result = get_vulnrichment_ssvc(vuln_id)
+            if result is None:
+                confusion_matrix[4, INDEXES[predicted_decision]] += 1
                 continue
+            actual_decision = get_decision(decision_tree, result, MISSION_WELL_BEING)
 
-            match = pattern.match(filename)
+            confusion_matrix[INDEXES[actual_decision], INDEXES[predicted_decision]] += 1
 
-            if match is None:
-                continue
-            model_name = match.group(1)
-
-            file_path = os.path.join(dirpath, filename)
-            with open(file_path, 'r') as f:
-                data = json.load(f)
-
-            if "vulnerabilities" not in data:
-                continue
-
-            for item in data["vulnerabilities"]:
-                vuln_id = item["vulnerability_id"]
-                predicted_decision = item["ssvc_decision"]
-
-                result = get_vulnrichment_ssvc(vuln_id)
-                if result is None:
-                    confusion_matrix[4, INDEXES[predicted_decision]] += 1
-                    continue
-                actual_decision = get_decision(decision_tree, result, MISSION_WELL_BEING)
-
-                confusion_matrix[INDEXES[actual_decision], INDEXES[predicted_decision]] += 1
-
-    return confusion_matrix
+    return confusion_matrices
 
 
 def load_decision_tree() -> dict:
@@ -106,7 +112,7 @@ def make_figure():
     plt.figure(figsize=(4, 3), layout="constrained")
 
 
-def plot_confusion_matrix(confusion_matrix):
+def plot_confusion_matrix(confusion_matrix: npt.NDArray, label: str):
     make_figure()
 
     midrange = (np.min(confusion_matrix) + np.max(confusion_matrix)) / 2
@@ -117,7 +123,7 @@ def plot_confusion_matrix(confusion_matrix):
         for col in range(confusion_matrix.shape[1]):
             item = int(confusion_matrix[row, col])
             color = "black" if item >= midrange else "white"
-            plt.text(col, row, item, ha="center", va="center", color=color)
+            plt.text(col, row, str(item), ha="center", va="center", color=color)
 
     plt.colorbar()
     plt.xlabel("Predicted decision")
@@ -125,8 +131,8 @@ def plot_confusion_matrix(confusion_matrix):
     plt.xticks(np.arange(4), LABELS)
     plt.yticks(np.arange(5), LABELS + ["Not listed"])
 
-    plt.savefig("confusion_matrix.pdf")
-    plt.savefig("confusion_matrix.png")
+    plt.savefig(f"confusion_matrix_{label}.pdf")
+    plt.savefig(f"confusion_matrix_{label}.png")
 
 
 def main():
@@ -134,8 +140,10 @@ def main():
         print("Please enter a directory name")
     dir_name = sys.argv[1]
 
-    confusion_matrix = get_confusion_matrix(dir_name)
-    plot_confusion_matrix(confusion_matrix)
+    confusion_matrices = get_confusion_matrices(dir_name)
+
+    for model_name, confusion_matrix in confusion_matrices.items():
+        plot_confusion_matrix(confusion_matrix, model_name)
 
 
 if __name__ == "__main__":

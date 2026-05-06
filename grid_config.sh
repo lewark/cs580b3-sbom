0e142affbec3290ddeaf7069ab37b76912579052
Everett Lewark <78086132+lewark@users.noreply.github.com>
2026-05-05 20:35:27 -0600

    Add all models/repeats to grid_config.sh

--- grid_config.sh
+++ grid_config.sh
@@ -3,8 +3,11 @@
 # List of models to test
 MODELS=( 
     "ministral-3:14b-cloud" 
-    "qwen3.5:397b-cloud" 
+    "qwen3.5:397b-cloud"
     "nemotron-3-super:cloud"
+    "glm-5.1:cloud"
+    "qwen3-next:80b-cloud"
+    "gemma4:31b-cloud"
 )
 
 # Associative array of software to test, format: ["name"]="url"
@@ -17,4 +20,4 @@ declare -A SOFTWARE=(
 )
 
 # Number of iterations to run for each model/software combination
-ITERATIONS=1
+ITERATIONS=3
